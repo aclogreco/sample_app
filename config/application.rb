9522802a8fa654c953e770316a6cf02aca226107
Anthony LoGreco <aclogreco@gmail.com>
2013-01-25 05:28:05 -0500

    Add line required by Heroku to application.rb

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Required by Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
