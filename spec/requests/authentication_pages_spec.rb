9f384853ceb72406b6a0a875f9cb023fac93d17c
Anthony LoGreco <aclogreco@gmail.com>
2013-12-31 01:14:58 -0500

    Chapter 8 - Exercise 1: Complete the refactoring of the sign in form.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,18 +7,17 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
     
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_selector('h1',    text: 'Sign In') }
+    it { should have_selector('title', text: 'Sign In') }
   end
   
   describe "signin" do
     before { visit signin_path }
     
     describe "with invalid information" do
-      before { click_button "Sign in" }
+      before { click_button "Sign In" }
       
-      it { should have_selector('title', text: 'Sign in') }
-#      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_selector('title', text: 'Sign In') }
       it { should have_error_message('Invalid') }
       
       describe "after visiting another page" do
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -12,7 +12,7 @@ include ApplicationHelper
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
-  click_button "Sign in"
+  click_button "Sign In"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
